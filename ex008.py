28cae45018444f758bdb2bedbb9c0f3cd4974501
DalahanX <66420044+DalahanX@users.noreply.github.com>
2021-12-30 20:32:02 -0300

    Exercício - Python 0.1
    Oitavo exercício que mostra várias medidas convertidas de metro para quilômetro, hectômetro, decâmetro e etc.

---
+++ ex008.py
@@ -0,0 +1,4 @@
+m = float(input("Digite um distância em metros: "))
+
+print(f"\nA medida de {m}m corresponde à:\n\n")
+print(f"{m / 1000}km \n{m / 100}hm \n{m / 10}dam \n{m * 10}dm \n{m * 100}cm \n{m * 1000}mm")
