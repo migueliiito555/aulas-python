c0fdc75e1f8a904e2291b0ab97efbb826e64cf75
DalahanX <66420044+DalahanX@users.noreply.github.com>
2021-12-30 20:29:20 -0300

    Exercício - Python 0.1
    Quarto exercício, imprime informações sobre um valor digitado pelo usuário.

---
+++ ex004.py
@@ -0,0 +1,10 @@
+palavra = input("Digite algo: ")
+
+print(f"O tipo primitivo desse valor é {type(palavra)}")
+print(f"Só tem espaços? {palavra.isspace()}")
+print(f"É um número? {palavra.isnumeric()}")
+print(f"É alfabético? {palavra.isalpha()}")
+print(f"É alfanumérico? {palavra.isalnum()}")
+print(f"Está em maiúsculas? {palavra.isupper()}")
+print(f"Está em minúsculas? {palavra.islower()}")
+print(f"Está capitalizada? {palavra.istitle()}")
