0ed206065ca4b460a48ed781a5f8bb2e8ed24132
DalahanX <66420044+DalahanX@users.noreply.github.com>
2021-12-31 12:13:37 -0300

    Exercício - Python 0.1
    Décimo terceiro exercício que aplica 15% de aumento em um salário inserido pelo usuário.

---
+++ ex013.py
@@ -0,0 +1,3 @@
+salario = float(input("Qual o salário do trabalhador? R$"))
+
+print(f"Com um salário de R${salario :.2f} e um aumento de 15%, seu salário agora é de R${salario + (salario * 0.15) :.2f}.")
