c7704efd88066b34b75a8e925387b553f4c6ce8a
DalahanX <66420044+DalahanX@users.noreply.github.com>
2021-12-30 23:51:29 -0300

    Exercício - Python 0.1
    Décimo primeiro exercício que calcula a quantidade de tinta necessário a cada 2m² para 1L de tinta, com dimensões digitadas pelo usuário.

---
+++ ex011.py
@@ -0,0 +1,6 @@
+c = float(input("Digite o comprimento da parede: "))
+a = float(input("Digite a altura da parede: "))
+
+print(f"\nSua parede possui as dimensões {c}mx{a}m, ou seja, {c * a}m².")
+
+print(f"\nSerá necessário {c * a / 2}L de tinta para pintar a parede.")
