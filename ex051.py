5faeba002bb1f48d1fdc40d99bff6a2fb95b3b1b
DalahanX <66420044+DalahanX@users.noreply.github.com>
2022-01-05 22:06:08 -0300

    Exercício - Python 0.1
    Quinquagésimo exercício que imprime na tela os 10 primeiros termos de uma PA com somente o primeiro termo e a razão da PA inseridas pelo usuário.

---
+++ ex051.py
@@ -0,0 +1,10 @@
+primeiroTermo = float(input("Qual o primeiro termo da PA? "))
+razao = float(input("E qual a razão? "))
+
+
+print()
+for i in range(0, 10):
+    print(primeiroTermo)
+
+
+    primeiroTermo += razao
